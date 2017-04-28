cc94a1c3e5edff7ec890369dece9e58e4c8c3fca
Julianne Costa <julesleighcosta@gmail.com>
2017-04-27 20:55:29 -0700

    Canvas project

---
+++ w5d5/canvas_practice/practice.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+  let canvas = document.getElementById('mycanvas');
+  let ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'red';
+  ctx.fillRect(10, 10, 500, 500);
+
+});
